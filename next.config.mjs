d75a59efc24fd46584b2dac5c59890404ecfae75
raunak mishra <raunakmishra6300@gmail.com>
2024-09-11 23:16:27 +0530

    setup lucia auth and zod validation

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
         staleTimes: {
             dynamic: 30,// 30 seconds, the client side cache will be invalidated after 30 seconds
         }
-    }
+    },
+    serverExternalPackages: [
+        "@node-rs/argon2",//described in lucia docs
+    ],
 };
 
 export default nextConfig;
