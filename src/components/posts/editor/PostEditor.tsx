6e0e9d4d7d3aebf3e6168763af15714e2c5dae86
raunak mishra <raunakmishra6300@gmail.com>
2024-11-13 15:55:26 +0530

    add post editor and delete post functionlity

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -9,9 +9,13 @@ import { useSession } from "@/app/(main)/SessionProvider";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import './styles.css';
+import { useSubmitPostMutation } from "./mutations";
+import LoadingButton from "@/components/LoadingButton";
 
 export default function PostEditor(){
     const {user} = useSession();
+
+    const mutation = useSubmitPostMutation();
   const editor = useEditor({
     extensions: [
         StarterKit.configure({
@@ -28,8 +32,12 @@ export default function PostEditor(){
     blockSeparator: "\n",
   }) || "";
 
-  async function onSubmit(){
-    await submitPost(input);
+   function onSubmit(){
+    mutation.mutate(input, {
+        onSuccess: () => {
+            editor?.commands.clearContent();
+        }
+    });
     editor?.commands.clearContent();//clear the editor after submitting
   }
 
@@ -45,13 +53,14 @@ export default function PostEditor(){
             />
              </div>
              <div className="flex justify-end">
-                <Button
+                <LoadingButton
                     onClick={onSubmit}
+                    loading={mutation.isPending}
                     disabled={!input.trim()}
                     className="min-w-20"
                     >
                     Post
-                </Button>
+                </LoadingButton>
              </div>
         </div>
     )
